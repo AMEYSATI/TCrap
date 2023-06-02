24d347a3d15631a0918b1657ab19a2fc8469eb94
AMEYSATI <78717195+AMEYSATI@users.noreply.github.com>
2023-06-02 17:59:03 +0530

    change made

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,13 @@
 /* eslint-disable no-unused-vars */
+import React from "react";
 import { HiMenuAlt4}  from 'react-icons/hi';
 import { AiOutlineClose } from 'react-icons/ai';
 
 import logo from '../../images/logo.png';
 
+const NavBarItem = ({ title, classprops }) => (
+    <li className={`mx-4 cursor-pointer ${classprops}`}>{title}</li>
+  );
 
 const Navbar = () => {
     return (
@@ -12,7 +16,12 @@ const Navbar = () => {
                 <img src={logo} alt="logo" className='w-32 cursor-pointer'/>
             </div>
             <ul className='text-white md:flex hidden list-none flex-row justify-between items-center flex-initial'>
-
+                    {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
+                        <NavBarItem key={item + index} title={item} />
+                    ))}
+                    <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
+                        Login
+                    </li>
             </ul>
         </nav>
     );
