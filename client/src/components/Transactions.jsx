fea689d9ec5d6a289fc8da1fe7ebc7bfaa1ed2cc
AMEYSATI <78717195+AMEYSATI@users.noreply.github.com>
2023-06-05 23:22:30 +0530

    changes made

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -50,9 +50,19 @@ const TransactionsCard = ({ addressTo, addressFrom, timestamp, message, keyword,
 
 
 const Transactions = () => {
+    const { transactions, currentAccount } = useContext(TransactionContext);
+  
     return (
-        <h1>Transactions</h1>
-    );
-}
+      <div className="flex w-full justify-center items-center 2xl:px-20 gradient-bg-transactions">
+        <div className="flex flex-col md:p-12 py-12 px-4">
+          {currentAccount ? (
+            <h3 className="text-white text-3xl text-center my-2">
+              Latest Transactions
+            </h3>
+          ) : (
+            <h3 className="text-white text-3xl text-center my-2">
+              Connect your account to see the latest transactions
+            </h3>
+          )}
 
 export default Transactions;
