0a512e95b82e7a223da33bc5b7a73e7e78ba35c9
AMEYSATI <78717195+AMEYSATI@users.noreply.github.com>
2023-06-06 11:00:37 +0530

    changes made

---
+++ smart_contract/contracts/Transactions.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract Transactions {
+    uint256 transactionCount;
+
+    event Transfer(address from, address receiver, uint amount, string message, uint256 timestamp,  string account, string keyword);
+  
+    struct TransferStruct {
+        address sender;
+        address receiver;
+        uint amount;
+        string message;
+        uint256 timestamp;
+        string account;
+        string keyword;
+    }
